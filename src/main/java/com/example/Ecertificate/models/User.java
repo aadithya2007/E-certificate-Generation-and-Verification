80a825a1f73e5b9c9ae64933e87c5318fc4fa2e7
aadithya2007 <aadithya2807@gmail.com>
2025-10-29 13:29:01 +0530

    user model completed

--- src/main/java/com/example/Ecertificate/models/User.java
+++ src/main/java/com/example/Ecertificate/models/User.java
@@ -1,4 +1,35 @@
 package com.example.Ecertificate.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 }
