eb577b7998805905380425050b9853c8e13441c3
aadithya2007 <aadithya2807@gmail.com>
2025-10-29 18:58:41 +0530

    certificate log get

---
+++ src/main/java/com/example/Ecertificate/controller/CertificateLogController.java
@@ -0,0 +1,27 @@
+package com.example.Ecertificate.controller;
+
+
+
+
+import com.example.Ecertificate.dto.LogRequestDTO;
+import com.example.Ecertificate.models.CertificateLog;
+import com.example.Ecertificate.view.CertificateLogServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/logs")
+public class CertificateLogController {
+
+    @Autowired
+    private CertificateLogServices logService;
+
+    @GetMapping
+    public List<CertificateLog> getAllLogs() {
+        return logService.getAllLogs();
+    }
+
+
+}
