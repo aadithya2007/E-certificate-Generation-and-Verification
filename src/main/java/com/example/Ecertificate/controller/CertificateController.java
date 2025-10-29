181ce41dc29ed093b4490c8ae1ca0c758a72ce34
aadithya2007 <aadithya2807@gmail.com>
2025-10-29 14:46:54 +0530

    create certificate

---
+++ src/main/java/com/example/Ecertificate/controller/CertificateController.java
@@ -0,0 +1,31 @@
+package com.example.Ecertificate.controller;
+
+
+
+
+import com.example.Ecertificate.models.Certificate;
+import com.example.Ecertificate.view.CertificateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/certificates")
+public class CertificateController {
+
+    @Autowired
+    private CertificateService certificateService;
+
+
+    @PostMapping
+    public Certificate createCertificate(@RequestBody Certificate certificate) {
+        return certificateService.createCertificateTemplate(certificate);
+    }
+
+
+}
