2bf85bb5ce9cbf20fac631843305c082a8146cb0
aadithya2007 <aadithya2807@gmail.com>
2025-10-29 13:00:37 +0530

    Initial commit

---
+++ src/main/java/com/example/Ecertificate/EcertificateApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ecertificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcertificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcertificateApplication.class, args);
+	}
+
+}
